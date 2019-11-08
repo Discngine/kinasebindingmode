57fc1a90a5279c6fd83f1b15b24dedfcc8089778
pschmidtke <peter.schmidtke@discngine.com>
2019-11-08 23:05:10 +0100

    first tests

--- testRdkit.py
+++ testRdkit.py
@@ -4,6 +4,8 @@ import sys
 from rdkit.Chem import AllChem
 import rdkit.Chem as Chem
 from rdkit import DataStructs
+import numpy as np
+from rdkit.Chem import MACCSkeys
 
 def getBindingModeFromSmiles(smiles):
     if(sum(type1.smiles==smiles)): return "type1"
@@ -17,49 +19,75 @@ type2=pd.read_csv("prepared_data/type2.csv")
 type2["type"]="type2"
 type1_2=pd.read_csv("prepared_data/type1_2.csv")
 type1_2["type"]="type1_2"
-smiles=list(type1.smiles)+list(type2.smiles)+list(type1_2.smiles)
+smiles=list(type1.smiles)+list(type2.smiles)#+list(type1_2.smiles)
 
 random.shuffle(smiles)
-split=int(len(smiles)/2)
+split=int(len(smiles)/1.7)
 train_data=smiles[:split]
 trainFps=[]
 for smi in train_data:
     try:
-        trainFps.append(AllChem.GetMorganFingerprint(Chem.MolFromSmiles(smi),2))
+        trainFps.append(AllChem.GetMorganFingerprint(Chem.MolFromSmiles(smi),3))
+        #trainFps.append(MACCSkeys.GenMACCSKeys(Chem.MolFromSmiles(smi)))
     except Exception:
+        trainFps.append("")
         pass
 
 test_data=smiles[split:]
 nFound=0
+nCorrectTotal=0
 nNotFound=0
+confusionDict={"type1_tp":0,"type1_fn":0,"type2_tp":0,"type2_fn":0}
 for idx,molSmile in enumerate(test_data):
     try:
         realBindingMode=getBindingModeFromSmiles(molSmile)
+        
         matchingTrainIdx=[]
         matchingSimilarities=[]
         m1 = Chem.MolFromSmiles(molSmile)
-        fp1 = AllChem.GetMorganFingerprint(m1,2)
+        fp1 = AllChem.GetMorganFingerprint(m1,3)
+        #fp1 = MACCSkeys.GenMACCSKeys(m1)
         for fpidx,trainFp in enumerate(trainFps):
-            similarity=DataStructs.DiceSimilarity(trainFp,fp1)
-            if(similarity>0.5):
-                matchingTrainIdx.append(fpidx)
-                matchingSimilarities.append(similarity)
+            if(trainFp!=""):
+                #similarity=DataStructs.FingerprintSimilarity(trainFp,fp1)
+                similarity=DataStructs.DiceSimilarity(trainFp,fp1)
+                if(similarity>0.5):
+                    matchingTrainIdx.append(fpidx)
+                    matchingSimilarities.append(similarity)
+                    #if realBindingMode=="type2":
+                    #    print(molSmile,train_data[fpidx])
         if(len(matchingTrainIdx)):
-            print("Found matching molecules for query molecule")
-            print(matchingSimilarities)
-            argmax=np.argmax(matchingSimilarities)
-            smilesIndex=matchingTrainIdx[argmax]
-            matchingSmiles=trainFps[smilesIndex]
-            bindingMode=getBindingModeFromSmiles(matchingSmiles)
-            print(bindingMode,realBindingMode)
+            nCorrect=0
+            for matchingTrainIndex in matchingTrainIdx:
+            
+                matchingSmiles=train_data[matchingTrainIndex]
+                bindingMode=getBindingModeFromSmiles(matchingSmiles)
+                #print(bindingMode,realBindingMode)
+                if bindingMode==realBindingMode:
+                    nCorrect+=1
+                    
+            #print(nCorrect/float(len(matchingTrainIdx)))
+            if(nCorrect/float((len(matchingTrainIdx)))>0.5):
+                nCorrectTotal+=1
+                confusionDict[realBindingMode+"_tp"]+=1
+            else:
+                confusionDict[realBindingMode+"_fn"]+=1
+                    
+
+
             nFound+=1
         else:
-            print("no match found")
+            
             nNotFound+=1
     except Exception as err:
         pass
-print(nFound)   
+print("Found")
+print(nFound)
+print("correct") 
+print(nCorrectTotal)
+print("not found")
 print(nNotFound)
+print(confusionDict)
     #except Exception:
     #   pass
 
