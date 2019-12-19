09d5caf04d5719c2479d1a33ea43839b60ff3861
Michael <michael.oberlin@discngine.com>
2019-12-19 16:11:00 +0100

    merge version with 2 types of annotations

--- scripts/getKinaseStrAnnotations/setup.py
+++ scripts/getKinaseStrAnnotations/setup.py
@@ -7,13 +7,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-        name='getKinaseProperties',
+        name='getKinaseStrAnnotations',
         version='0.1.0',
         description='Python script for kinase structural features processing, in order to classify potential binding modes',
         lon_description=readme,
         author='Anne-Sophie',
         author_email='',
-        url='https://github.com/Discngine/kinasebindingmode/getKinaseProperties',
+        url='https://github.com/Discngine/kinasebindingmode/getKinaseStrAnnotations',
         license=license,
         package=find_packages(exclude=('tests', 'docs'))
 )
