9d00e638dde08ddbb86c3d376463b4bb4b02de4f
pschmidtke <peter.schmidtke@discngine.com>
2019-12-20 22:00:08 +0100

    plotting mess

--- results/generatePlots.R
+++ results/generatePlots.R
@@ -9,9 +9,42 @@ ordered=r50[order(-r50$nFound,-r50$MCC),]
 #plot(ordered[,"similarityThreshold"],ordered[,"nFound"]/nData)
 ordered$similarityThreshold <- as.factor(ordered$similarityThreshold)
 
-tanimoto_top_radius_1=r[r[,"FingerprintRadius"]==1 & r[,"SimilarityMetric"]="TanimotoSimilarity" & r[,"evaluationMode"]="top",]
-plot(tanimoto_top_radius_1[,""])
 
-p=ggplot(ordered, aes(x=similarityThreshold, y=nFound)) + 
-  geom_violin() + geom_boxplot(width=0.1)
+createPlot<-function(data){
+  p=ggplot(data, aes(similarityThreshold)) + 
+  geom_line(aes(y=MCC,colour="MCC"),size=2) +
+  geom_line(aes(y=F1,colour="F1"),size=2) +
+  geom_line(aes(y=BA,colour="BA"),size=2) +
+  geom_line(aes(y=nFound/nData,colour="Ratio Found"),size=2) + 
+  geom_errorbar(aes(ymin=MCC-sdMCC, ymax=MCC+sdMCC), width=.01,color="grey") +
+  geom_errorbar(aes(ymin=F1-sdF1, ymax=F1+sdF1), width=.01,color="grey") +
+  geom_errorbar(aes(ymin=BA-sdBA, ymax=BA+sdBA), width=.01,color="grey") +
+  geom_errorbar(aes(ymin=nFound/nData-sdnFound/nData, ymax=nFound/nData+sdnFound/nData), width=.01,,color="grey") +
+  labs(y = "",
+                x = "Similarity Threshold",
+                colour = "Parameter") + 
+               scale_color_brewer(palette="Dark2")+theme_minimal()+
+                theme(legend.position="bottom")
+
+  return(p)
+
+
+}
+
+
+
+tanimoto_top_radius_1=r[r[,"FingerprintRadius"]==1 & r[,"SimilarityMetric"]=="TanimotoSimilarity" & r[,"evaluationMode"]=="top",]
+p=createPlot(tanimoto_top_radius_1)
+# p=ggplot(tanimoto_top_radius_1, aes(similarityThreshold)) + 
+#   geom_line(aes(y=MCC,colour="MCC"),color="blue") +
+#   geom_line(aes(y=F1,colour="F1"),color="green") +
+#   geom_line(aes(y=BA,colour="BA"),color="orange" ) +
+#   geom_line(aes(y=nFound/nData,colour="Ratio Found"),color="red" ) + 
+#   labs(y = "",
+#                 x = "Similarity Threshold",
+#                 colour = "Parameter")
 print(p)
+
+#p=ggplot(ordered, aes(x=similarityThreshold, y=nFound)) + 
+#  geom_violin() + geom_boxplot(width=0.1)
+#print(p)
